40745f57f6385c601207515195a71e186a6a1aaa
emils <emils@gmail.com>
2025-10-01 15:46:57 +0300

    8.punkts

--- submit.php
+++ submit.php
@@ -1,43 +1,46 @@
 <?php
-$servername = "book_review_01102025";
-$username = "book_review_user_01102025@localhost";
+$servername = "localhost";
+$username = "book_review_user_011102025";
 $password = "password";
-// Create connection
-// $conn = mysqli_connect($servername, $username, $password);
+$database = "book_review_01102025";
 
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-// echo "Connected successfully";
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
+
+
+// 
 ?>
 
 
 
+//
 <!DOCTYPE html>
-<html lang="en">
+// <html lang="en">
+
+//
 
 <head>
+    //
     <meta charset="UTF-8">
+    //
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    // <title>Document</title>
+    //
 </head>
 
+//
+
 <body>
-    <p><?php echo ($_POST['username']); ?></p>
-    <p> dati veiksmīgi nosūtītu serverim</p>
-    <a href="index">back to start</a>
-</body>
+    // <p><?php echo ($_POST['username']); ?></p>
+    // <p> dati veiksmīgi nosūtītu serverim</p>
+    // <a href="index">back to start</a>
+    // </body>
+
+//
 
 </html>
