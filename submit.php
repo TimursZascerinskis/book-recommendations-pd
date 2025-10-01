7e4038ee9ed9fe98c69703d211323d1f453dfddc
emils <emils@gmail.com>
2025-10-01 15:33:23 +0300

    7.punkts

--- submit.php
+++ submit.php
@@ -1,3 +1,30 @@
+<?php
+$servername = "book_review_01102025";
+$username = "book_review_user_01102025@localhost";
+$password = "password";
+// Create connection
+// $conn = mysqli_connect($servername, $username, $password);
+
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
+
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
