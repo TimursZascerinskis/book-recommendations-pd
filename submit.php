48e659bd64c3cdcce5c7e2d07159969bf737f7e2
emils <emils@gmail.com>
2025-10-01 16:00:41 +0300

    10.punkts

--- submit.php
+++ submit.php
@@ -1,4 +1,5 @@
 <?php
+
 $servername = "localhost";
 $username = "book_review_user_011102025";
 $password = "password";
@@ -6,41 +7,46 @@ $database = "book_review_01102025";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
 
+$firstName = $_POST['username'];
+$lastName = $_POST['surname'];
+$bookTitle = $_POST['title'];
+$reviewText = $_POST['apraksts'];
 
-// 
+
+
+$fullName = $firstName . ' ' . $lastName;
+
+$stmt = $conn->prepare("INSERT INTO book_review (full_name, book_title, review_text, rating) VALUES (?, ?, ?, ?)");
+
+$stmt->bind_param("sssi", $fullName, $bookTitle, $reviewText, $rating);
+
+
+$stmt->close();
+$conn->close();
 ?>
 
 
 
-//
-<!DOCTYPE html>
-// <html lang="en">
 
-//
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    //
     <meta charset="UTF-8">
-    //
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    // <title>Document</title>
-    //
+    <title>Document</title>
 </head>
 
-//
-
 <body>
-    // <p><?php echo ($_POST['username']); ?></p>
-    // <p> dati veiksmīgi nosūtītu serverim</p>
-    // <a href="index">back to start</a>
-    // </body>
-
-//
+    <p><?php echo ($_POST['username']); ?></p>
+    <p> dati veiksmīgi nosūtītu serverim</p>
+    <a href="index">back to start</a>
+</body>
 
 </html>
